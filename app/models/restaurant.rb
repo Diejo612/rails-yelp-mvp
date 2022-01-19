87be59404d158a4ef375e442d54d957563700ee0
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-01-19 13:41:18 -0500

    listo

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"].freeze
+
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: VALID_CATEGORIES }
 end
